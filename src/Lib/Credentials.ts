738c25777f495138fcda1abbb328b0786a9bc527
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2022-11-13 11:48:36 +0530

    Initialized firebase

--- src/Lib/Credentials.ts
+++ src/Lib/Credentials.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-	apiKey: "AIzaSyCikANApfq-onBavpAwyDPIYJznZnF0_3E",
-	authDomain: "project-weddy.firebaseapp.com",
-	projectId: "project-weddy",
-	storageBucket: "project-weddy.appspot.com",
-	messagingSenderId: "194706914076",
-	appId: "1:194706914076:web:957119529b7d2e3c8b58d9",
-	measurementId: "G-84CBFNB3F0",
+	apiKey: "AIzaSyCNqbpppVyArDYRhPDgwv3bg6OpmRqXKGk",
+	authDomain: "project-wentz.firebaseapp.com",
+	projectId: "project-wentz",
+	storageBucket: "project-wentz.appspot.com",
+	messagingSenderId: "920320375165",
+	appId: "1:920320375165:web:77bd4f4906ea1e81c6b414",
+	measurementId: "G-Y40YMGYBES",
 }
